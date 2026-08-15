a58a22639e7d69baebb9a4194104ecd87ba4cc1b
isik29 <isikinugur29@gmail.com>
2026-08-15 16:39:18 +0300

    Change bot activity name to 'servant'

--- src/config/bot.js
+++ src/config/bot.js
@@ -23,7 +23,7 @@ export const botConfig = {
     // 5 = Competing
     activities: [
       {
-        name: "Custom Status", // required by Discord API, not shown in the client
+        name: "servant", // required by Discord API, not shown in the client
         state: "im just lois",     // this is what people actually see
         type: 4,               // Custom
       },
